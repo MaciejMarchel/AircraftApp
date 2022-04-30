7295e4f2b6c24d6569c9fcbee42a3c9b27ed5a55
Maciej <maciejmarchel14@gmail.com>
2022-04-30 20:22:48 +0100

    Had to change airType and airMake validation to use while loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -69,17 +69,61 @@ FUNCTIONS FOR THE MENU TO WORK
 
 fun addAircraft(){
     //logger.info { "Add an aircraft" }
+
+
+
     val airName = readNextLine("Enter a name of the aircraft: ")
-    val airType = readNextLine("Enter the type of the aircraft: ")
+
+
+    /*
+    Had to use while loop to validate airType and airMake for adding and updating further down
+    Old validation is commented out in the function(s)
+     */
+    var airType = ""
+
+    var type2 = "TurboJet"
+    var type1 = "TurboProp"
+
+
+    while(airType.lowercase() != type1.lowercase() || airType.lowercase() != type2.lowercase()) {
+
+        airType = readNextLine("Enter the type (TurboJet or TurboProp) of the aircraft: ")
+
+        if(airType.lowercase() == type1.lowercase() || airType.lowercase() == type2.lowercase()) {
+            break
+        } else {
+            println("Please enter either TurboJet or TurboProp")
+        }
+    }
+
     val airCost = readNextDouble("Enter the cost of the aircraft: ")
-    val airMake = readNextLine("Enter the make of the aircraft: ")
+
+    var airMake = ""
+
+    var make1 = "Textron Aviation"
+    var make2 = "Boeing"
+    var make3 = "Airbus"
+    var make4 = "Comac"
+
+    while(airMake.lowercase() != make1.lowercase() || airMake.lowercase() != make2.lowercase() || airMake.lowercase() != make3.lowercase() || airMake.lowercase() != make4.lowercase()) {
+
+        airMake = readNextLine("Enter the make (Textron Aviation, Boeing, Airbus, Comac) of the aircraft: ")
+
+        if(airMake.lowercase() == make1.lowercase() || airMake.lowercase() == make2.lowercase() || airMake.lowercase() == make3.lowercase() || airMake.lowercase() == make4.lowercase()) {
+            break
+        } else {
+            println("Please enter either Textron Aviation, Boeing, Airbus or Comac")
+        }
+    }
+
     val isAdded = aircraftAPI.add(Aircraft(airName, airType, airCost, airMake, false))
 
     if (isAdded) {
-        check(isValidType(typeToCheck = airType)) { "Please input the correct data for the field of Type (TurboProp, TurboJet)" } //checks for the correct type inputted
-        check(isValidMake(makeToCheck = airMake)) { "Please input the correct data for the field of Make (Textron Aviation, Boeing, Airbus, Comac)" } //checks for the correct make inputted
-    } else {
         println("Added Successfully")
+        //check(isValidType(typeToCheck = airType)) { "Please input the correct data for the field of Type (TurboProp, TurboJet)" } //checks for the correct type inputted
+        //check(isValidMake(makeToCheck = airMake)) { "Please input the correct data for the field of Make (Textron Aviation, Boeing, Airbus, Comac)" } //checks for the correct make inputted
+    } else {
+        println("Failure to add aircraft")
     }
 }
 
@@ -164,16 +208,52 @@ fun updateAircraft(){
         val indexToUpdate = readNextInt("Enter the index of the aircraft to update: ")
         if (aircraftAPI.isValidIndex(indexToUpdate)) {
             val airName = readNextLine("Enter a name for the aircraft: ")
-            val airType = readNextLine("Enter the type for the aircraft: ")
-            val airMake = readNextLine("Enter the make for the aircraft: ")
+
+            //Validation for airType
+            var airType = ""
+
+            var type2 = "TurboJet"
+            var type1 = "TurboProp"
+
+            while(airType.lowercase() != type1.lowercase() || airType.lowercase() != type2.lowercase()) {
+
+                airType = readNextLine("Enter the type (TurboJet or TurboProp) of the aircraft: ")
+
+                if(airType.lowercase() == type1.lowercase() || airType.lowercase() == type2.lowercase()) {
+                    break
+                } else {
+                    println("Please enter either TurboJet or TurboProp")
+                }
+            }
+
+            //Validation for airMake
+            var airMake = ""
+
+            var make1 = "Textron Aviation"
+            var make2 = "Boeing"
+            var make3 = "Airbus"
+            var make4 = "Comac"
+
+            while(airMake.lowercase() != make1.lowercase() || airMake.lowercase() != make2.lowercase() || airMake.lowercase() != make3.lowercase() || airMake.lowercase() != make4.lowercase()) {
+
+                airMake = readNextLine("Enter the make (Textron Aviation, Boeing, Airbus, Comac) of the aircraft: ")
+
+                if(airMake.lowercase() == make1.lowercase() || airMake.lowercase() == make2.lowercase() || airMake.lowercase() == make3.lowercase() || airMake.lowercase() == make4.lowercase()) {
+                    break
+                } else {
+                    println("Please enter either Textron Aviation, Boeing, Airbus or Comac")
+                }
+            }
+
             val airCost = readNextDouble("Enter the cost of the aircraft: ")
 
             //pass the index of the aircraft and new aircraft details to AircraftAPI for updating & to check success
             if (aircraftAPI.updateAircraft(indexToUpdate, Aircraft(airName,airType,airCost,airMake, false))){
-                check(isValidType(typeToCheck = airType)) { "Please input the correct data for the field of Type (TurboProp, TurboJet)" } //checks for the correct type inputted
-                check(isValidMake(makeToCheck = airMake)) { "Please input the correct data for the field of Make (Textron Aviation, Boeing, Airbus, Comac)" } //checks for the correct make inputted
-            } else {
                 println("Update Successful")
+                //check(isValidType(typeToCheck = airType)) { "Please input the correct data for the field of Type (TurboProp, TurboJet)" } //checks for the correct type inputted
+                //check(isValidMake(makeToCheck = airMake)) { "Please input the correct data for the field of Make (Textron Aviation, Boeing, Airbus, Comac)" } //checks for the correct make inputted
+            } else {
+                println("Update Failed")
             }
         } else {
             println("There are no aircraft for this index number")
--- src/main/kotlin/utils/TypeMakeUtility.kt
+++ src/main/kotlin/utils/TypeMakeUtility.kt
@@ -2,6 +2,15 @@ package utils
 
 object TypeMakeUtility {
 
+    /*
+    ___________________________________________________________________________________________________________________
+    | Had to move away from this type of validation, the way I had to implement it would cause the app to crash/shut   |
+    | down whenever the user entered the wrong text in and tried adding the aircraft, I had to basically switch        |
+    | to using a while loop to validate those fields as having the app crash/shut off wasn't user-friendly and could   |
+    | cause potential issues for users who didn't save their progress.                                                 |
+    ___________________________________________________________________________________________________________________
+     */
+
     //NOTE: JvmStatic annotation means that the categories variable is static (i.e. we can reference it through the class
     //      name; we don't have to create an object of TypeMakeUtility to use it.
     @JvmStatic
